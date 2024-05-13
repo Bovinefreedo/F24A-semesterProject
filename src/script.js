fae98750ed72e2957128c5405b211829ee37b65a
dbaredon <tonytruong550@gmail.com>
2024-05-13 09:39:45 +0200

    Tonys Files Sug.

--- src/script.js
+++ src/script.js
@@ -1,1 +1,14 @@
-console.log("hallo stygge");
+let lastScrollTop = 0;
+
+window.addEventListener("scroll", function() {
+    let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+    
+    // Check if scrolling down from the first page
+    if (currentScroll > lastScrollTop && window.scrollY > window.innerHeight) {
+        document.querySelector("header").style.top = "-100px"; // Hide navbar
+    } else {
+        document.querySelector("header").style.top = "0"; // Show navbar
+    }
+
+    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+});
