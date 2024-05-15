9332168ea1fe696e673a12646e6f417af3f4cb0f
dbaredon <tonytruong550@gmail.com>
2024-05-15 18:08:02 +0200

    fortsættelse af quiz samt graf.

--- src/script.js
+++ src/script.js
@@ -13,3 +13,51 @@ window.addEventListener("scroll", function() {
     lastScrollTop = currentScroll;
     
 });
+
+const quizData = [
+    {
+        question: "What is the capital of France?",
+    a: "Paris",
+    b: "Aarhus",
+    correct: "b",
+    },
+
+    {
+        question: "Hvad hedder Berkant til fornavn",
+    a: "John",
+    b: "Jon",
+    correct: "a",
+    },
+]
+
+const quiz = document.querySelector(".quiz-body");
+const answerEl = document.querySelector(".answer")
+const quiestionEl = document.getElementById("quiestion");
+const footerEl = document.querySelector (".quizfooter");
+const quizDetailEl = documentquerySelector(".quiz-details");
+
+const a_txt = document.getElementById ("a_txt");
+const b_txt = document.getElementById ("b_txt");
+
+let currentQuiz = 0;
+let score = 0;
+
+loadQuiz ();
+
+function loadQuiz (){
+    deselectAnswer ();
+    const currentQuizData = quizData [currentQuiz];
+
+    quiestionEl.innerText = currentQuizData.question;
+    a_txt.innerText = currentQuizData.a;
+    b_txt.innerText = currentQuizData.b;
+
+    quizDetailEl.innerHTML = `<p>${currentQuiz + 1} of ${quizData.length}</p>`;
+}
+
+
+function deselectAnswer (){
+    answerEl.forEach ((answerEl)=>{
+        (answerEl.checked) = false;
+    });
+}
