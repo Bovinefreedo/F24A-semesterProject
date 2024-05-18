69978c4747b8a59838136e74471257e9ec6ec476
GioBeshara <giobeshara4@gmail.com>
2024-05-18 15:28:13 +0200

    Created a line chart for Danish Energy Consumption

--- src/script.js
+++ src/script.js
@@ -17,75 +17,75 @@ const svg = d3
   .append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
+// Create the tooltip
+const toolTip = d3.select("body").append("div").attr("class", "toolTip");
 
-  // Create the tooltip
-  const toolTip = d3.select("body").append("div").attr("class", "toolTip");
-
-  // Data
-  const data = [
-  { date: new Date("1965"), value: 43307.869491 },
-  { date: new Date("1966"), value: 45623.36122 },
-  { date: new Date("1967"), value: 47341.248466 },
-  { date: new Date("1968"), value: 50203.3882 },
-  { date: new Date("1969"), value: 53570.320537 },
-  { date: new Date("1970"), value: 56984.876954 },
-  { date: new Date("1971"), value: 59321.212442 },
-  { date: new Date("1972"), value: 62498.72939 },
-  { date: new Date("1973"), value: 66152.347567 },
-  { date: new Date("1974"), value: 66572.543126 },
-  { date: new Date("1975"), value: 66886.819792 },
-  { date: new Date("1976"), value: 70454.327149 },
-  { date: new Date("1977"), value: 73046.036659 },
-  { date: new Date("1978"), value: 76009.502364971 },
-  { date: new Date("1979"), value: 78627.85507594299 },
-  { date: new Date("1980"), value: 77946.874138333 },
-  { date: new Date("1981"), value: 77624.550771333 },
-  { date: new Date("1982"), value: 77205.68184816299 },
-  { date: new Date("1983"), value: 78453.68916542099 },
-  { date: new Date("1984"), value: 82178.642776558 },
-  { date: new Date("1985"), value: 84306.37807554999 },
-  { date: new Date("1986"), value: 86197.38983113 },
-  { date: new Date("1987"), value: 89201.510200013 },
-  { date: new Date("1988"), value: 92560.12045344301 },
-  { date: new Date("1989"), value: 94387.01442234 },
-  { date: new Date("1990"), value: 95604.4724825 },
-  { date: new Date("1991"), value: 96274.60217140001 },
-  { date: new Date("1992"), value: 96821.87972900001 },
-  { date: new Date("1993"), value: 97590.49469440001 },
-  { date: new Date("1994"), value: 98825.79225530002 },
-  { date: new Date("1995"), value: 101077.8755395 },
-  { date: new Date("1996"), value: 103972.08091649998 },
-  { date: new Date("1997"), value: 105047.6394912 },
-  { date: new Date("1998"), value: 105711.14717760001 },
-  { date: new Date("1999"), value: 107538.6629546 },
-  { date: new Date("2000"), value: 110356.5341164 },
-  { date: new Date("2001"), value: 111457.3254784 },
-  { date: new Date("2002"), value: 113842.337514 },
-  { date: new Date("2003"), value: 117889.013508 },
-  { date: new Date("2004"), value: 123741.62616299998 },
-  { date: new Date("2005"), value: 127801.18816 },
-  { date: new Date("2006"), value: 131415.59607 },
-  { date: new Date("2007"), value: 135446.26037600002 },
-  { date: new Date("2008"), value: 137036.114603 },
-  { date: new Date("2009"), value: 134829.023426 },
-  { date: new Date("2010"), value: 141325.55716000003 },
-  { date: new Date("2011"), value: 144578.91247 },
-  { date: new Date("2012"), value: 146655.71709 },
-  { date: new Date("2013"), value: 149220.31066 },
-  { date: new Date("2014"), value: 150798.41086 },
-  { date: new Date("2015"), value: 152054.08198 },
-  { date: new Date("2016"), value: 153884.57384 },
-  { date: new Date("2017"), value: 157110.15800000002 },
-  { date: new Date("2018"), value: 161402.7497 },
-  { date: new Date("2019"), value: 163163.344 },
-  { date: new Date("2020"), value: 157357.8056 },
-  { date: new Date("2021"), value: 165946.1632 },
-  { date: new Date("2022"), value: 167787.67500000002 },
+// Data
+const data = 
+[
+  { date: new Date("1965"), value: 166.20826872 },
+  { date: new Date("1966"), value: 189.05940166 },
+  { date: new Date("1967"), value: 190.12346472000002 },
+  { date: new Date("1968"), value: 198.66685966699998 },
+  { date: new Date("1969"), value: 228.04658789 },
+  { date: new Date("1970"), value: 242.98941677 },
+  { date: new Date("1971"), value: 229.63516177 },
+  { date: new Date("1972"), value: 242.34226477 },
+  { date: new Date("1973"), value: 234.46956477 },
+  { date: new Date("1974"), value: 213.31388576999998 },
+  { date: new Date("1975"), value: 210.23947666700002 },
+  { date: new Date("1976"), value: 231.44317277599998 },
+  { date: new Date("1977"), value: 236.18926765999998 },
+  { date: new Date("1978"), value: 239.618583581 },
+  { date: new Date("1979"), value: 239.138624723 },
+  { date: new Date("1980"), value: 232.96088166700002 },
+  { date: new Date("1981"), value: 209.247351383 },
+  { date: new Date("1982"), value: 199.994370833 },
+  { date: new Date("1983"), value: 190.113586385 },
+  { date: new Date("1984"), value: 193.292690826 },
+  { date: new Date("1985"), value: 217.8157011 },
+  { date: new Date("1986"), value: 222.64347924999998 },
+  { date: new Date("1987"), value: 214.6407237 },
+  { date: new Date("1988"), value: 212.30624447 },
+  { date: new Date("1989"), value: 195.81452697999998 },
+  { date: new Date("1990"), value: 201.96101393 },
+  { date: new Date("1991"), value: 232.85370569999998 },
+  { date: new Date("1992"), value: 218.136789906 },
+  { date: new Date("1993"), value: 231.30373301999998 },
+  { date: new Date("1994"), value: 248.05591808 },
+  { date: new Date("1995"), value: 243.61966967 },
+  { date: new Date("1996"), value: 243.61966967 },
+  { date: new Date("1997"), value: 262.99795685945 },
+  { date: new Date("1998"), value: 253.6913200285 },
+  { date: new Date("1999"), value: 246.0488783504 },
+  { date: new Date("2000"), value: 236.5193922986 },
+  { date: new Date("2001"), value: 237.2164963832 },
+  { date: new Date("2002"), value: 236.7554476083 },
+  { date: new Date("2003"), value: 255.23254668299998 },
+  { date: new Date("2004"), value: 241.6517519175 },
+  { date: new Date("2005"), value: 227.5990561063 },
+  { date: new Date("2006"), value: 251.5625387385 },
+  { date: new Date("2007"), value: 238.63370215830003 },
+  { date: new Date("2008"), value: 226.6213594088 },
+  { date: new Date("2009"), value: 214.5770196565 },
+  { date: new Date("2010"), value: 227.93804901800002 },
+  { date: new Date("2011"), value: 214.15298283700002 },
+  { date: new Date("2012"), value: 199.192198536 },
+  { date: new Date("2013"), value: 207.78352041 },
+  { date: new Date("2014"), value: 200.00195200000002 },
+  { date: new Date("2015"), value: 194.821310932 },
+  { date: new Date("2016"), value: 197.83895819999998 },
+  { date: new Date("2017"), value: 198.832132254 },
+  { date: new Date("2018"), value: 196.31110973 },
+  { date: new Date("2019"), value: 192.071242873 },
+  { date: new Date("2020"), value: 171.1597073 },
+  { date: new Date("2021"), value: 188.206108008 },
+  { date: new Date("2022"), value: 189.396615154 },
 ];
 
 // Set the x and y domains
 x.domain(d3.extent(data, (d) => d.date));
-y.domain([40000, d3.max(data, (d) => d.value)]);
+y.domain([160, d3.max(data, (d) => d.value)]);
 
 // Add the x axis
 svg
@@ -94,18 +94,16 @@ svg
   .style("font-size", "12px")
   .call(d3.axisBottom(x).ticks(d3.timeYear.every(10)));
 
-
-// Add the y axis 
+// Add the y axis
 svg
   .append("g")
   .style("font-size", "12px")
   .call(d3.axisLeft(y).tickFormat((d) => d + " Twh"));
 
-
 // created a horizontol grid
 svg
   .selectAll("yGrid")
-  .data(y.ticks(d3.max(data, (d) => d.value) / 25000).slice(1))
+  .data(y.ticks(d3.max(data, (d) => d.value) / 25).slice(1))
   .join("line")
   .attr("x1", 0)
   .attr("x2", width)
@@ -127,7 +125,6 @@ svg
   .attr("stroke", "lightgrey")
   .attr("stroke-width", 0.5);
 
-
 // Add y axis label
 svg
   .append("text")
@@ -141,16 +138,14 @@ svg
   .style("font-family", "sans-serif")
   .text("Terawatt i timen");
 
-
-  // Add the line
-  const line = d3
+// Add the line
+const line = d3
   .line()
   .x((d) => x(d.date))
   .y((d) => y(d.value));
 
-
-  // Add the circle element
-  const circle = svg
+// Add the circle element
+const circle = svg
   .append("circle")
   .attr("r", 0)
   .attr("fill", "steelblue")
@@ -158,47 +153,45 @@ svg
   .attr("opacity", 0.7)
   .style("pointer-events", "none");
 
-
-  // Create the svg element and append it to the chart container. This gives the ability to move the mouse anywhere on the chart and get the appropriate point
-  const listeningRect = svg
+// Create the svg element and append it to the chart container. This gives the ability to move the mouse anywhere on the chart and get the appropriate point
+const listeningRect = svg
   .append("rect")
   .attr("width", width)
   .attr("height", height);
 
+// Give the event when the mouse moves. The bisector takes the position of the mouse and finds the nearest datapoint.
+listeningRect.on("mousemove", function (event) {
+  const [xCoord] = d3.pointer(event, this);
+  const bisectDate = d3.bisector((d) => d.date).left;
+  const x0 = x.invert(xCoord);
+  const i = bisectDate(data, x0, 1);
+  const d0 = data[i - 1];
+  const d1 = data[i];
+  const d = x0 - d0.date > d1.date - x0 ? d1 : d0;
+  const xPos = x(d.date);
+  const yPos = y(d.value);
 
-  // Give the event when the mouse moves. The bisector takes the position of the mouse and finds the nearest datapoint.
-  listeningRect.on("mousemove", function (event) {
-    const [xCoord] = d3.pointer(event, this);
-    const bisectDate = d3.bisector((d) => d.date).left;
-    const x0 = x.invert(xCoord);
-    const i = bisectDate(data, x0, 1);
-    const d0 = data[i - 1];
-    const d1 = data[i];
-    const d = x0 - d0.date > d1.date - x0 ? d1 : d0;
-    const xPos = x(d.date);
-    const yPos = y(d.value);
+  // Update circle position
+  circle.attr("cx", xPos).attr("cy", yPos);
 
-    // Update circle position
-    circle.attr("cx", xPos).attr("cy", yPos);
+  // Add the transition for the circle radius
+  circle.transition().duration(50).attr("r", 5);
 
-    // Add the transition for the circle radius
-    circle.transition().duration(50).attr("r", 5);
+  const year = d.date.getFullYear();
 
-    const year = d.date.getFullYear();
+  // Add the tooltip
+  toolTip
+    .style("display", "block")
+    .style("left", `${xPos + 100}px`)
+    .style("top", `${yPos + 50}px`)
+    .html(
+      `<strong>År:</strong> ${year}<br><strong>Forbrug:</strong> ${
+        d.value !== undefined ? d.value.toFixed(0) + " Twh" : "N/A"
+      }`
+    );
+});
 
-    // Add the tooltip
-    toolTip
-      .style("display", "block")
-      .style("left", `${xPos + 100}px`)
-      .style("top", `${yPos + 50}px`)
-      .html(
-        `<strong>År:</strong> ${year}<br><strong>Forbrug:</strong> ${
-          d.value !== undefined ? d.value.toFixed(0) + " Twh" : "N/A"
-        }`
-      );
-  });
-
-  // Removes the tooltip when mouse is not in the chart
+// Removes the tooltip when mouse is not in the chart
 listeningRect.on("mouseleave", function () {
   circle.transition().duration(50).attr("r", 0);
 
@@ -221,67 +214,3 @@ svg
     const length = this.getTotalLength();
     return d3.interpolate(`0,${length}`, `${length}, ${length}`);
   });
-
-/*
-Danish energi consumption from 1960-2022
-  [
-    { date: new Date("1965"), value: 166.20826872 },
-    { date: new Date("1966"), value: 189.05940166 },
-    { date: new Date("1967"), value: 190.12346472000002 },
-    { date: new Date("1968"), value: 198.66685966699998 },
-    { date: new Date("1969"), value: 228.04658789 },
-    { date: new Date("1970"), value: 242.98941677 },
-    { date: new Date("1971"), value: 229.63516177 },
-    { date: new Date("1972"), value: 242.34226477 },
-    { date: new Date("1973"), value: 234.46956477 },
-    { date: new Date("1974"), value: 213.31388576999998 },
-    { date: new Date("1975"), value: 210.23947666700002 },
-    { date: new Date("1976"), value: 231.44317277599998 },
-    { date: new Date("1977"), value: 236.18926765999998 },
-    { date: new Date("1978"), value: 239.618583581 },
-    { date: new Date("1979"), value: 239.138624723 },
-    { date: new Date("1980"), value: 232.96088166700002 },
-    { date: new Date("1981"), value: 209.247351383 },
-    { date: new Date("1982"), value: 199.994370833 },
-    { date: new Date("1983"), value: 190.113586385 },
-    { date: new Date("1984"), value: 193.292690826 },
-    { date: new Date("1985"), value: 217.8157011 },
-    { date: new Date("1986"), value: 222.64347924999998 },
-    { date: new Date("1987"), value: 214.6407237 },
-    { date: new Date("1988"), value: 212.30624447 },
-    { date: new Date("1989"), value: 195.81452697999998 },
-    { date: new Date("1990"), value: 201.96101393 },
-    { date: new Date("1991"), value: 232.85370569999998 },
-    { date: new Date("1992"), value: 218.136789906 },
-    { date: new Date("1993"), value: 231.30373301999998 },
-    { date: new Date("1994"), value: 248.05591808 },
-    { date: new Date("1995"), value: 243.61966967 },
-    { date: new Date("1996"), value: 243.61966967 },
-    { date: new Date("1997"), value: 262.99795685945 },
-    { date: new Date("1998"), value: 253.6913200285 },
-    { date: new Date("1999"), value: 246.0488783504 },
-    { date: new Date("2000"), value: 236.5193922986 },
-    { date: new Date("2001"), value: 237.2164963832 },
-    { date: new Date("2002"), value: 236.7554476083 },
-    { date: new Date("2003"), value: 255.23254668299998 },
-    { date: new Date("2004"), value: 241.6517519175 },
-    { date: new Date("2005"), value: 227.5990561063 },
-    { date: new Date("2006"), value: 251.5625387385 },
-    { date: new Date("2007"), value: 238.63370215830003 },
-    { date: new Date("2008"), value: 226.6213594088 },
-    { date: new Date("2009"), value: 214.5770196565 },
-    { date: new Date("2010"), value: 227.93804901800002 },
-    { date: new Date("2011"), value: 214.15298283700002 },
-    { date: new Date("2012"), value: 199.192198536 },
-    { date: new Date("2013"), value: 207.78352041 },
-    { date: new Date("2014"), value: 200.00195200000002 },
-    { date: new Date("2015"), value: 194.821310932 },
-    { date: new Date("2016"), value: 197.83895819999998 },
-    { date: new Date("2017"), value: 198.832132254 },
-    { date: new Date("2018"), value: 196.31110973 },
-    { date: new Date("2019"), value: 192.071242873 },
-    { date: new Date("2020"), value: 171.1597073 },
-    { date: new Date("2021"), value: 188.206108008 },
-    { date: new Date("2022"), value: 189.396615154 },
-  ];
-*/
