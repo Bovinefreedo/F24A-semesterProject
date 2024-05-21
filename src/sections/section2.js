f3539346557eba170ea3d9f9ef01bfcbf2732b23
dbaredon <tonytruong550@gmail.com>
2024-05-21 11:21:50 +0200

    update

---
+++ src/sections/section2.js
@@ -0,0 +1,22 @@
+export function createSection2(){
+    const section5 = document.getElementById("section2Content");
+    const canvas5 = document.createElement("div");
+    section5.appendChild(canvas5);
+    canvas5.id="canvas2";
+    canvas5.className = "background"
+    const apiUrl = 'http://localhost:4000/getPopulation';
+    fetch(apiUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+          console.log(data)
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+  }
+   
