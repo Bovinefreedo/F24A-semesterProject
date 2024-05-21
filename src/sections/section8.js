d1ba83a7cdc2145a198f1ff8f18b767a66d701fb
Frank Skorstengaard <1612-au651342@user.noreply.gitlab.au.dk>
2024-05-21 21:48:10 +0200

    Before new edition of treemap

--- src/sections/section8.js
+++ src/sections/section8.js
@@ -36,68 +36,6 @@ export function createSection8() {
       // Create the tooltip
       const toolTip = d3.select("body").append("div").attr("class", "toolTip");
 
-      /* Data
-const data = [
-  { date: new Date("1965"), value: 43307.869491 },
-  { date: new Date("1966"), value: 45623.36122 },
-  { date: new Date("1967"), value: 47341.248466 },
-  { date: new Date("1968"), value: 50203.3882 },
-  { date: new Date("1969"), value: 53570.320537 },
-  { date: new Date("1970"), value: 56984.876954 },
-  { date: new Date("1971"), value: 59321.212442 },
-  { date: new Date("1972"), value: 62498.72939 },
-  { date: new Date("1973"), value: 66152.347567 },
-  { date: new Date("1974"), value: 66572.543126 },
-  { date: new Date("1975"), value: 66886.819792 },
-  { date: new Date("1976"), value: 70454.327149 },
-  { date: new Date("1977"), value: 73046.036659 },
-  { date: new Date("1978"), value: 76009.502364971 },
-  { date: new Date("1979"), value: 78627.85507594299 },
-  { date: new Date("1980"), value: 77946.874138333 },
-  { date: new Date("1981"), value: 77624.550771333 },
-  { date: new Date("1982"), value: 77205.68184816299 },
-  { date: new Date("1983"), value: 78453.68916542099 },
-  { date: new Date("1984"), value: 82178.642776558 },
-  { date: new Date("1985"), value: 84306.37807554999 },
-  { date: new Date("1986"), value: 86197.38983113 },
-  { date: new Date("1987"), value: 89201.510200013 },
-  { date: new Date("1988"), value: 92560.12045344301 },
-  { date: new Date("1989"), value: 94387.01442234 },
-  { date: new Date("1990"), value: 95604.4724825 },
-  { date: new Date("1991"), value: 96274.60217140001 },
-  { date: new Date("1992"), value: 96821.87972900001 },
-  { date: new Date("1993"), value: 97590.49469440001 },
-  { date: new Date("1994"), value: 98825.79225530002 },
-  { date: new Date("1995"), value: 101077.8755395 },
-  { date: new Date("1996"), value: 103972.08091649998 },
-  { date: new Date("1997"), value: 105047.6394912 },
-  { date: new Date("1998"), value: 105711.14717760001 },
-  { date: new Date("1999"), value: 107538.6629546 },
-  { date: new Date("2000"), value: 110356.5341164 },
-  { date: new Date("2001"), value: 111457.3254784 },
-  { date: new Date("2002"), value: 113842.337514 },
-  { date: new Date("2003"), value: 117889.013508 },
-  { date: new Date("2004"), value: 123741.62616299998 },
-  { date: new Date("2005"), value: 127801.18816 },
-  { date: new Date("2006"), value: 131415.59607 },
-  { date: new Date("2007"), value: 135446.26037600002 },
-  { date: new Date("2008"), value: 137036.114603 },
-  { date: new Date("2009"), value: 134829.023426 },
-  { date: new Date("2010"), value: 141325.55716000003 },
-  { date: new Date("2011"), value: 144578.91247 },
-  { date: new Date("2012"), value: 146655.71709 },
-  { date: new Date("2013"), value: 149220.31066 },
-  { date: new Date("2014"), value: 150798.41086 },
-  { date: new Date("2015"), value: 152054.08198 },
-  { date: new Date("2016"), value: 153884.57384 },
-  { date: new Date("2017"), value: 157110.15800000002 },
-  { date: new Date("2018"), value: 161402.7497 },
-  { date: new Date("2019"), value: 163163.344 },
-  { date: new Date("2020"), value: 157357.8056 },
-  { date: new Date("2021"), value: 165946.1632 },
-  { date: new Date("2022"), value: 167787.67500000002 },
-];*/
-
       // Set the x and y domains
       x.domain(d3.extent(data, (d) => d.date));
       y.domain([40000, d3.max(data, (d) => d.value)]);
@@ -192,7 +130,7 @@ const data = [
         // Add the transition for the circle radius
         circle.transition().duration(50).attr("r", 5);
 
-        const year = d.date.getFullYear();
+        const year = d.date;
 
         // Add the tooltip
         toolTip
