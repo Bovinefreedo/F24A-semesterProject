085f3c4b19b795533e1b093d8786650e73477de3
GioBeshara <giobeshara4@gmail.com>
2024-05-23 11:52:40 +0200

    update

--- src/script.js
+++ src/script.js
@@ -1,8 +1,6 @@
 import {createSection5} from "./sections/section5.js"
-import {createSection8 } from "./sections/section8.js";
 
 createSection5();
-createSection8();
 let lastScrollTop = 0;
 
 window.addEventListener("scroll", function() {
