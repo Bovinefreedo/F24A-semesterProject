7ff98c219dd8745c9edeb8c57b552480c16af32d
dbaredon <tonytruong550@gmail.com>
2024-05-28 17:27:47 +0200

    berkant

--- src/dataQuery.js
+++ src/dataQuery.js
@@ -407,7 +407,7 @@ const getYearEnergyUseRegion = (request, response) => {
 };
 
 const getEnergyMixCountry = (request, response) => {
-  pool.query("SELECT amountInTWH AS value, year, energyName AS axsis, countryName FROM energyUseCountry INNER JOIN energyType ON energyType.energyTypeID = energyUseCountry.energyTypeID INNER JOIN country ON country.countryID = energyUseCountry.countryID WHERE year = 2022 AND ( countryName ILIKE 'DENMARK' OR countryName ILIKE 'GERMANY' )", (error, results) => {
+  pool.query("SELECT amountInTWH AS value, year, energyName AS axsis, countryName FROM energyUseCountry INNER JOIN energyType ON energyType.energyTypeID = energyUseCountry.energyTypeID INNER JOIN country ON country.countryID = energyUseCountry.countryID WHERE year BETWEEN 1990 AND 2022 AND ( countryName ILIKE 'DENMARK')", (error, results) => {
     if (error) {
       throw error;
     }
