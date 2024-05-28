abddd9f554d994788ddd4100d0762c063c296607
Frank Skorstengaard <1612-au651342@user.noreply.gitlab.au.dk>
2024-05-28 06:50:20 +0200

    now the input makes the speedometers and counters react

--- src/sections/section5.js
+++ src/sections/section5.js
@@ -75,16 +75,43 @@ export function createSection5(){
 
         for(let i=0; i<3; i++){
             for(let j=0; j<2; j++){
-                const counter = new RollingCounter(gaugeContainer, (j*30+50).toString()+"%", (i*25+34.5).toString()+"%", 0)
+                const counter = new RollingCounter( ""+i+j,gaugeContainer, (j*30+50).toString()+"%", (i*25+34.5).toString()+"%", 0)
                 counterList.push(counter);
             }
         }
 
-
+        console.log((Number.parseFloat(data[2].usedenergy)));
         gaugesList[0].needle.animateOn(gaugesList[0].chart, .70);
         gaugesList[1].needle.animateOn(gaugesList[1].chart, .20);
         slider.oninput = function() {
-          yearDiv.innerHTML = this.value;
+            let value = Number(this.value)            
+            yearDiv.innerHTML = value;
+            let yearlyChange; 
+            for(let i=0; i<data.length; i++){
+            if(data[i].year == value){
+                if(data[i].energysupertype === "renewable"){
+                    yearlyChange = calculateYearlyChange(data[i].change);
+                    gaugesList[0].needle.animateOn(gaugesList[0].chart, data[i].usedenergy/140000);
+                    gaugesList[1].needle.animateOn(gaugesList[1].chart, yearlyChange)
+                    counterList[0].changeNumber(data[i].usedenergy);
+                    counterList[1].changeNumber(data[i].change);
+                }
+                if(data[i].energysupertype === "nuclear"){
+                    yearlyChange = calculateYearlyChange(data[i].change);
+                    gaugesList[2].needle.animateOn(gaugesList[2].chart, data[i].usedenergy/140000);
+                    gaugesList[3].needle.animateOn(gaugesList[3].chart, yearlyChange)
+                    counterList[2].changeNumber(data[i].usedenergy);
+                    counterList[3].changeNumber(data[i].change);
+                }
+                if(data[i].energysupertype === "fossil"){
+                    yearlyChange = calculateYearlyChange(data[i].change);
+                    gaugesList[4].needle.animateOn(gaugesList[4].chart, data[i].usedenergy/140000);
+                    gaugesList[5].needle.animateOn(gaugesList[5].chart, yearlyChange)
+                    counterList[4].changeNumber(data[i].usedenergy);
+                    counterList[5].changeNumber(data[i].change);
+                }
+            }
+          }
         }
       })
       .catch(error => {
@@ -207,26 +234,28 @@ function Gauge(id, parrentDiv, needleLength, needleWidth, positionTop, positionL
 }
 
 //id require #, it is made to take a div. Initial value is a number
-function RollingCounter(parentDiv, positionTop, positionLeft, intialValue){
+function RollingCounter(ID, parentDiv, positionTop, positionLeft, intialValue){
     this.start = intialValue;
     let counter = document.createElement('div');
+    this.counter = counter
     counter.style.fontSize = "25px"
     parentDiv.appendChild(counter);
     counter.style.left = positionLeft;
     counter.style.top = positionTop;
     counter.style.position = "absolute";
     counter.style.transform = "translate(-50%,-50%)";
+    counter.id = "counter"+ID;
     counter.innerHTML = intialValue;
     this.changeNumber = (newValue) => {
-        d3.select(counter).transition()
+        console.log("this far OK")
+        d3.select(this.counter).transition()
         .tween("text", () => {
-        const interpolator = d3.interpolateNumber(start, newValue);
+        const interpolator = d3.interpolateNumber(this.start, newValue);
         return function(t) {
             d3.select(this).text(Math.round(interpolator(t))) 
     }
     })
     .duration(1000);
-    this.start = newValue;
   }
 }
 
@@ -299,6 +328,7 @@ function gaugeContainerElements(div){
     gaugeContainer.appendChild(row1Label);
     row1Label.style.top = "37%"
     row1Label.style.left = "5%"
+    row1Label.style.fontSize = "15px"
     row1Label.innerHTML = "Totalt energi forbrug"
     let row2Label = document.createElement('div');
     labelStyle(row2Label);
@@ -306,34 +336,50 @@ function gaugeContainerElements(div){
     row2Label.style.top = "70%"
     row2Label.style.left = "5%"
     row2Label.innerHTML = "Øgning i forbrug siden sidste år"
+    row2Label.style.fontSize = "15px"
     let column1Label = document.createElement('div');
     labelStyle(column1Label);
     gaugeContainer.appendChild(column1Label);
-    column1Label.style.top = "27%";
+    column1Label.style.top = "24%";
     column1Label.style.left = "28%";
     column1Label.innerHTML = "Vedvarende Energi"
+    column1Label.style.fontSize = "15px"
     let column2Label = document.createElement('div');
     labelStyle(column2Label);
     gaugeContainer.appendChild(column2Label);
-    column2Label.style.top = "27%";
+    column2Label.style.top = "24%";
     column2Label.style.left = "53%";
     column2Label.innerHTML = "Atomkraft"
+    column2Label.style.fontSize = "15px"
     let column3Label = document.createElement('div');
     gaugeContainer.appendChild(column3Label);
     labelStyle(column3Label);
-    column3Label.style.top = "27%";
+    column3Label.style.top = "24%";
     column3Label.style.left = "78%";
     column3Label.innerHTML = "Forsil energi"
+    column3Label.style.fontSize = "15px"
     let yearLabel = document.createElement('div');
     labelStyle(yearLabel);
     gaugeContainer.appendChild(yearLabel);
     yearLabel.style.top = "12%";
     yearLabel.style.left = "28%";
     yearLabel.innerHTML = "Vælg år"
+    yearLabel.style.fontSize = "15px"
 }
 
 function labelStyle(div){
     div.style.position = "absolute";
     div.style.width = "12%";
     div.style.height = "5%";
+}
+
+function calculateYearlyChange(data){
+    if(data>4000){
+        return 1;
+    } else if(data<-500){
+        return 0;
+    } else {
+        return ((data+500)/4500)
+    }
+
 }
