8adae4b46a16030b0272a3ef28832fa32465b2ca
dbaredon <tonytruong550@gmail.com>
2024-05-30 12:02:18 +0200

    caspers

--- src/dataQuery.js
+++ src/dataQuery.js
@@ -407,15 +407,12 @@ const getYearEnergyUseRegion = (request, response) => {
 };
 
 const getEnergyMixCountry = (request, response) => {
-  pool.query(
-    "SELECT amountInTWH AS value, year, energyName AS axis, countryName FROM energyUseCountry INNER JOIN energyType ON energyType.energyTypeID = energyUseCountry.energyTypeID INNER JOIN country ON country.countryID = energyUseCountry.countryID WHERE year BETWEEN 1990 AND 2022 AND (countryName ILIKE 'DENMARK') ORDER BY year ASC",
-    (error, results) => {
-      if (error) {
-        throw error;
-      }
-      response.status(200).json(results.rows);
+  pool.query("SELECT amountInTWH AS value, year, energyName AS axis, countryName FROM energyUseCountry INNER JOIN energyType ON energyType.energyTypeID = energyUseCountry.energyTypeID INNER JOIN country ON country.countryID = energyUseCountry.countryID WHERE year = 2022 AND ( countryName ILIKE 'DENMARK' OR countryName ILIKE 'GERMANY' OR countryName ILIKE 'SWEDEN' OR countryName ILIKE 'CHINA' OR countryName ILIKE 'UNITED STATES' OR countryName ILIKE 'RUSSIA' OR countryName ILIKE 'WORLD')", (error, results) => {
+    if (error) {
+      throw error;
     }
-  );
+    response.status(200).json(results.rows);
+  });
 };
 
 
